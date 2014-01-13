5e24dc15b4c30dae24e224f53942ae25d4189387
Fergus Leen <leenf@tcd.ie>
2014-01-13 05:10:37 +0100

    Basic layout

--- app.js
+++ app.js
@@ -14,22 +14,59 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.session());
+
+
+// Message middleware so we can do flash messages
+app.use(function(req, res, next){
+  console.log("1 ",req.session);
+
+  var msgs = req.session.messages || [];
+
+  // empty or "flush" the messages so they don't build up
+  delete req.session.messages;
+
+  res.locals({
+    messages: msgs,
+    hasMessages: !! msgs.length
+  });
+
+  next();
+});
+
+app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
+app.get('/madmini/signed', routes.signed);
 app.get('/', routes.index);
+
+app.use(function(err, req, res, next){
+  // treat as 404
+  if (~err.message.indexOf('not found')) return next();
+
+  // log it
+  console.error(err.stack);
+
+  // error page
+  res.status(500).render('5xx');
+});
+
+// assume 404 since no middleware responded
+app.use(function(req, res, next){
+  res.status(404);//.render('404', { url: req.originalUrl });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@
  */
 
 exports.index = function(req, res){
-  res.render('index');
+  res.render('index' );
+};
+
+exports.signed = function(req, res) {
+  req.session.messages = [{type: 'alert-success', message: 'Sign up complete!'}];
+  res.redirect('/');
 };

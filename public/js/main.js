72c26895f3e0e7560238f93885b4eb3ccf848889
Fergus Leen <leenf@tcd.ie>
2014-01-18 19:34:47 +0000

    Fixed cards and added room names

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,51 @@
 
+var urlParam = function(name){
+    var results = new RegExp('[\\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
+    return results ? results[1] : false;
+}
+var m = Math.random() + 1;
+console.log(m);
+console.log((m).toString(36));
+console.log((m).toString(36).substring(2) );
+
+var room =  urlParam('room') || (Math.random() + 1).toString(36).substring(2,7);
+
+console.log('room',room);
+
+var webrtc = new SimpleWebRTC({
+  // the id/element dom element that will hold "our" video
+  localVideoEl: 'localVideo',
+  // the id/element dom element that will hold remote videos
+  remoteVideosEl: 'remoteVideos',
+  // immediately ask for camera access
+  // disable this for signup page
+  autoRequestMedia: false,
+});
+
+// we have to wait until it's ready
+webrtc.on('readyToCall', function () {
+  // you can name it anything
+  // this should be randomised for the demo site
+  webrtc.joinRoom(room);
+  document.getElementById('shareRoom').innerHTML = 'Add a friend : <a href="'+window.location.host+'?room='+room+'">'+window.location.host+'?room='+room+'</a>';
+});
+
+// this is called before the handlePeerStreamAdded() function
+webrtc.on('peerStreamAdded', function (peer) {
+});
+
+// This gets called when a remote video is added - see handlePeerStreamAdded() function
+webrtc.on('videoAdded', function (video, peer) {
+  $(video).parent().addClass("card");
+  $(video).parent().append("<img src='/img/hearthstone-final-click-me-sharp-text.png'></img>");
+  $(video).parent().animate(
+    {
+      duration: 500,
+      specialEasing: "slideIn"
+    });
+  //$(video).addClass("smallVideo");
+});
+
+var start = function() {
+  webrtc.startLocalVideo();
+}

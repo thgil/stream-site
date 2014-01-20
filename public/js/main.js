93df90943409b943346e38552a33e857c6e1bc09
David Leen <dleen@uw.edu>
2014-01-19 16:12:35 -0800

    Added query string to routes.
    Added handling for query string.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
+autoplay = autoplay == "false" ? false : true;
+
+if (autoplay) {
+  document.getElementById('localImage').src = "/img/hearthstone-blank-no-text.png";  
+}
+
 var webrtc = new SimpleWebRTC({
   // the id/element dom element that will hold "our" video
   localVideoEl: 'localVideo',
@@ -5,7 +11,7 @@ var webrtc = new SimpleWebRTC({
   remoteVideosEl: 'remoteVideos',
   // immediately ask for camera access
   // disable this for signup page
-  autoRequestMedia: false,
+  autoRequestMedia: autoplay,
 });
 
 // we have to wait until it's ready
@@ -13,11 +19,10 @@ webrtc.on('readyToCall', function () {
   // you can name it anything
   // this should be randomised for the demo site
   webrtc.joinRoom(room);
-  // html content in a variable?
-  // var cont = '<a href="'+window.location.host+'?room='+room+'">'+window.location.host+'?room='+room+'</a>';
-  $("#localImage").popover({ title: 'Add a friend!', content: 'Share link should go here. Also need to change the css styling for this.', placement: 'left', trigger: 'manual' });
+  $("#localImage").popover({ title: 'Add a friend!', html: true, content: '<a href="#" id="justatest">This is link</a>', placement: 'left', trigger: 'manual' });
   $("#localImage").popover('show');
-  // document.getElementById('shareRoom').innerHTML = 'Add a friend : <a href="'+window.location.host+'?room='+room+'">'+window.location.host+'?room='+room+'</a>';
+
+  document.getElementById('justatest').href = window.location.host+'/'+room;
 });
 
 // this is called before the handlePeerStreamAdded() function
@@ -26,6 +31,7 @@ webrtc.on('peerStreamAdded', function (peer) {
 
 // This gets called when a remote video is added - see handlePeerStreamAdded() function
 webrtc.on('videoAdded', function (video, peer) {
+  $("#localImage").popover('hide');
   $(video).parent().addClass("card");
   $(video).parent().append("<img src='/img/hearthstone-blank-no-text.png'></img>");
   $(video).parent().append("<div class='cardText'><h3>Hello hello hello</h3></div>");
@@ -34,7 +40,6 @@ webrtc.on('videoAdded', function (video, peer) {
       duration: 500,
       specialEasing: "slideIn"
     });
-  //$(video).addClass("smallVideo");
 });
 
 var start = function() {
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,14 @@
  */
 
 exports.index = function(req, res){
-
   var room =  (Math.random() + 1).toString(36).substring(2,7);
-  res.redirect('/'+ room);
+  res.redirect('/' + room + '?autoplay=false');
 };
 
 exports.room = function(req, res) {
-
   var room = req.params.room;
-  res.render('index', {room: room});
+  var autoplay = req.query.autoplay;
+  res.render('index', {room: room, autoplay: autoplay});
 };
 
 exports.webrtc = function(req, res) {
